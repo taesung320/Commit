fbdbe29fe5ff82c5ef3b6b81f4a3ee96adc7f589
taesung320 <taesung320@gmail.com>
2018-05-25 20:17:50 +0900

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,23 @@
+//
+//  main.cpp
+//  cal
+//
+//  Created by saesol on 2018. 5. 25..
+//  Copyright © 2018년 scit3. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int num1,num2;
+    char A;
+    cout<<"첫번째 수";cin>>num1;
+    cout<<"연산자";cin>>A;
+    cout<<"두번쨰 수";cin>>num2;
+    
+    if(A=='+')
+    {
+        cout<<num1+num2;
+    }
+}
